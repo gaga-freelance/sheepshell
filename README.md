36c78850f8283e0bd22ba17431e4cf5e3184b1cf
Aaron <aaron.williams@codeenigma.com>
2018-07-12 22:29:42 +0200

    Version 1.0.0 yay!

--- README.md
+++ README.md
@@ -1,2 +1,52 @@
-# sheepshell
-The Giantsheep front-end boilerplate and workflow 
+# SheepShell
+
+The Giantsheep front-end boilerplate and workflow
+
+---
+
+## Getting it working
+
+* Install Gulp globally
+
+/If you have installed Gulp previously then run this otherwise skip this step/
+'$ npm rm --global gulp'
+
+*Run this*
+''$ npm install --global gulp-cli'
+
+* Install all those lovely packages
+
+'$ npm install'
+
+* Run the thing
+
+'$ gulp'
+
+* That's it!
+
+Your site should be serving at localhost:3000
+
+/The default gulp task is running browserSync and watching for changes in the app directory/
+
+### Other tasks
+
+* Image optimisation
+
+'$ gulp images'
+
+This optimises images throughout. Takes bare time though!
+
+*  Clean up the dist folder
+
+'$ gulp clean:dist'
+
+Because we don't like extra crap
+
+---
+
+
+Bundled grid system is from [Simple Grid](https://simplegrid.io)
+
+Holla if anything looks silly and you'd like to let me know :)
+
+[Giantsheep](giantsheep.net)
---
+++ gulpfile.js
@@ -0,0 +1,55 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+var useref = require('gulp-useref');
+var uglify = require('gulp-uglify');
+var gulpIf = require('gulp-if');
+var imagemin = require('gulp-imagemin');
+var del = require('del');
+
+gulp.task('sass', function(){
+  return gulp.src('dist/scss/**/*.scss')
+    .pipe(sass()) // Using gulp-sass
+    .pipe(gulp.dest('app/assets/css'))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
+});
+
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: 'app'
+    },
+  })
+})
+
+gulp.task('default', ['browserSync', 'sass'], function (){
+  gulp.watch('dist/scss/**/*.scss', ['sass']);
+  gulp.watch('app/*.html', browserSync.reload);
+  gulp.watch('app/js/**/*.js', browserSync.reload);
+});
+
+gulp.task('useref', function(){
+  return gulp.src('app/*.html')
+    .pipe(useref())
+    .pipe(gulp.dest('dist'))
+});
+
+gulp.task('useref', function(){
+  return gulp.src('app/*.html')
+    .pipe(useref())
+    // Minifies only if it's a JavaScript file
+    .pipe(gulpIf('*.js', uglify()))
+    .pipe(gulp.dest('dist'))
+});
+
+gulp.task('images', function(){
+  return gulp.src('app/images/**/*.+(png|jpg|gif|svg)')
+  .pipe(imagemin())
+  .pipe(gulp.dest('dist/images'))
+});
+
+gulp.task('clean:dist', function() {
+  return del.sync('dist');
+})
